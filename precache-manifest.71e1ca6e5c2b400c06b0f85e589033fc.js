5f204293f80e5e8553130dfcb32dec30a2711d0c
Dmytro Tkachuk <tkachukdimon@gmail.com>
2020-09-05 19:08:12 +0300

    deploy

---
+++ precache-manifest.71e1ca6e5c2b400c06b0f85e589033fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba293562aab060771c6e21c6d7649deb",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "815f82618c3ff4775db5",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.fc84348c.chunk.css"
+  },
+  {
+    "revision": "ccf8a5b50b6ee4482189",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.35887bb4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.35887bb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815f82618c3ff4775db5",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.cfad48f7.chunk.js"
+  },
+  {
+    "revision": "9d1a26fb56dbe9d98a08",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b0a8c527.js"
+  }
+]);
